a409ad39d24eb67d1db5f53374e6479b1dc4b2bd
DevPplforce <devajyoti@pplforce.com>
2024-06-03 17:58:35 +0530

    back ground changer application completed

--- bg_changer/src/App.jsx
+++ bg_changer/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bgColor,setbgColor] = useState('yellow')
+
+  function changeColor(color) {
+    setbgColor(color);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full h-screen duration-100' style={{ backgroundColor: bgColor }}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button className='ouline-none px-10 py-1 rounded-full shadow-lg text-white bg-red-500' onClick={()=> changeColor('red')}>red</button>
+            <button className='ouline-none px-10 py-1 rounded-full shadow-lg text-white bg-green-500' onClick={()=> changeColor('green')}>green</button>
+            <button className='ouline-none px-10 py-1 rounded-full shadow-lg text-white bg-blue-500' onClick={()=> changeColor('blue')}>blue</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
